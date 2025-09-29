61a7e44e606f023b6c873498aea611fcc5c7702e
Sashangy Kumar <sashangy.kumar@atos.net>
2025-09-29 15:55:14 +0530

    Modified terraform code

--- modules/project_services/outputs.tf
+++ modules/project_services/outputs.tf
@@ -1,3 +1,7 @@
 output "enabled_services" {
-value = [for k in google_project_service.apis : k.service]
+  description = "List of all services enabled by this module."
+  value = concat(
+    [for s in google_project_service.core   : s.service],
+    [for s in google_project_service.others : s.service],
+  )
 }
