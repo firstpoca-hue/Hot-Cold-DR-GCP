f0f8d1ecf4c0a5b4b3b78f41464b230b01dec43a
Vishnu Vardhan <pvsvv1994@outlook.com>
2025-10-07 13:49:40 +0530

    two

--- root/variable.tf
+++ root/variable.tf
@@ -1,25 +1,106 @@
-# 
-
 variable "project_id" {
-  type        = string
-  description = "GCP Project ID"
+  type = string
+}
+
+variable "pattern" {
+  type = string
 }
 
 variable "region_a" {
-  type        = string
-  description = "Hot (primary) region"
+  type = string
 }
 
 variable "region_b" {
-  type        = string
-  description = "Cold (secondary) region"
-}
-
-variable "cluster_name_prefix" {
-  type    = string
-  default = "drp-cluster"
+  type = string
 }
 
 variable "artifact_registry_location" {
+  type = string
+}
 
+variable "cluster_name_prefix" {
+  type = string
+}
+
+variable "cluster_prefix" {
+  type = string
+}
+
+variable "artifact_repo_location" {
+  type = string
+}
+
+variable "artifact_registry_repo" {
+  type = string
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "subnets" {
+  type = list(object({
+    network_cidr = string
+    region       = string
+  }))
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "gke_service_account_name" {
+  type = string
+}
+
+variable "private_subnet_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "bastion_service_account_name" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_secondary_range_name" {
+  type = string
+}
+
+variable "services_secondary_range_name" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "node_machine_type" {
+  type = string
+}
+
+variable "node_disk_size" {
+  type = number
+}
+
+variable "cluster_secondary_range" {
+  type = string
+}
+
+variable "services_secondary_range" {
+  type = string
+}
+
+variable "master_subnet_cidr" {
+  type = string
+}
+
+variable "gke_account_email" {
+  type = string
 }
