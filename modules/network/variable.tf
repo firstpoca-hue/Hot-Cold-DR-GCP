03171cddccde5ceb9f732882ce452e475a5c2a88
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:24:21 +0530

    Update variable.tf

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,21 +1,29 @@
-# 
-
 variable "project_id" {
+  description = "The ID of the project in which to create the network."
   type        = string
-  description = "The project ID to deploy the network"
 }
 
-variable "network_name" {
+variable "vpc_name" {
+  description = "Name of the VPC network."
   type        = string
-  default     = "gke-vpc"
 }
 
-variable "region_a" {
-  type        = string
-  description = "Primary region"
+variable "regions" {
+  description = "Map of regions to be used for subnets and NAT routers."
+  type = object({
+    primary   = string
+    secondary = string
+  })
 }
 
-variable "subnet_cidr_a" {
-  type        = string
-  default     = "10.10.0.0/20"
+variable "subnet_cidrs" {
+  description = "Custom CIDR ranges for subnets per region (optional override)."
+  type = object({
+    primary   = optional(string, "10.10.0.0/20")
+    secondary = optional(string, "10.20.0.0/20")
+  })
+  default = {
+    primary   = "10.10.0.0/20"
+    secondary = "10.20.0.0/20"
+  }
 }
