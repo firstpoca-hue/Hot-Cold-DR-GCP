98376756b2c00ce72e248284f6290ac7fdf9ffa3
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:22:35 +0530

    Update outputs.tf

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -1,9 +1,8 @@
-# output "workspace_id" {
-#   value       = google_monitoring_workspace.workspace.id
-#   description = "ID of the monitoring workspace"
-# }
-
 output "alert_policy_id" {
   value       = google_monitoring_alert_policy.cpu_alert.id
   description = "ID of the alert policy"
+}
+
+output "uptime_check_id" { 
+value = google_monitoring_uptime_check_config.http.id 
 }
