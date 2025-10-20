d4b95e83fb381f48d1f39c7ad08caee547c7e0e1
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:23:58 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,7 +1,24 @@
-output "network_name" {
-  value = google_compute_network.vpc.name
+
+output "vpc_self_link" {
+  value = google_compute_network.vpc.self_link
 }
 
-output "subnet_name" {
-  value = google_compute_subnetwork.subnet_a.name
+output "vpc_id" {
+  value = google_compute_network.vpc.id
+}
+
+output "subnet_primary_self_link" {
+  value = google_compute_subnetwork.subnets["primary"].self_link
+}
+
+output "subnet_secondary_self_link" {
+  value = google_compute_subnetwork.subnets["secondary"].self_link
+}
+
+output "subnet_primary_name" {
+  value = google_compute_subnetwork.subnets["primary"].name
+}
+
+output "subnet_secondary_name" {
+  value = google_compute_subnetwork.subnets["secondary"].name
 }
