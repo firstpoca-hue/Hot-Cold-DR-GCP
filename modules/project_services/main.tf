15b4aaa736be8b6b947b1a8808c49ea3a6e9bad5
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:24:43 +0530

    Update main.tf

--- modules/project_services/main.tf
+++ modules/project_services/main.tf
@@ -1,29 +1,23 @@
+terraform {
+  required_providers { google = { source = "hashicorp/google" } }
+}
+
 resource "google_project_service" "services" {
   for_each = toset([
     "compute.googleapis.com",
     "container.googleapis.com",
-    "artifactregistry.googleapis.com",
+    "servicenetworking.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "sqladmin.googleapis.com",
+    "run.googleapis.com",
+    "monitoring.googleapis.com",
+    "logging.googleapis.com",
+    "secretmanager.googleapis.com",
     "iam.googleapis.com",
-    "dns.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "pubsub.googleapis.com"
   ])
   project = var.project_id
   service = each.value
-  disable_on_destroy = false
-}
-
-resource "google_artifact_registry_repository" "docker_repo" {
-  project       = var.project_id
-  location      = var.artifact_registry_location != "" ? var.artifact_registry_location : var.region_a
-  repository_id = "gke-docker-repo"
-  description   = "Multi-region Artifact Registry for container images"
-  format        = "DOCKER"
-
-  depends_on = [google_project_service.services]
-}
-
-resource "google_service_account" "gke_nodes" {
-  project      = var.project_id
-  account_id   = "gke-nodes"
-  display_name = "GKE Nodes Service Account"
 }
