0ab44dcefdc767b6d0bb9c5d5dd4690784198706
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:21:39 +0530

    Update variable.tf

--- modules/gke/variable.tf
+++ modules/gke/variable.tf
@@ -1,77 +1,52 @@
+# 
+
+# GCP project where the GKE cluster will be created
 variable "project_id" {
-  description = "GCP project id"
+  description = "The ID of the project in which resources will be created."
   type        = string
 }
 
+# Region or zone for the GKE cluster
 variable "region" {
-  description = "GCP region where the cluster will be created"
+  description = "The region where the GKE cluster will be deployed."
   type        = string
 }
 
+# Cluster name
 variable "cluster_name" {
-  description = "Name of the GKE cluster"
+  description = "The name of the GKE cluster."
   type        = string
 }
 
+# Network (VPC) to which the cluster is attached
 variable "network" {
-  description = "VPC network name (self link or name)"
+  description = "The VPC network for the GKE cluster."
   type        = string
 }
 
+# Subnetwork (subnet) to which the cluster nodes are attached
 variable "subnetwork" {
-  description = "Subnetwork name"
+  description = "The subnetwork for the GKE cluster."
   type        = string
 }
 
-variable "initial_node_count" {
-  description = "Initial node count for node pool"
-  type        = number
-  default     = 1
-}
-
-variable "min_nodes" {
-  description = "Minimum nodes for autoscaling"
-  type        = number
-  default     = 1
-}
-
-variable "max_nodes" {
-  description = "Maximum nodes for autoscaling"
-  type        = number
-  default     = 3
-}
-
-variable "node_machine_type" {
-  description = "Machine type for nodes"
-  type        = string
-  default     = "e2-standard-4"
-}
-
-variable "enable_private_cluster" {
-  description = "Whether to create private nodes"
+# Flag to control if this cluster should be created (true/false)
+variable "enable" {
+  description = "If false, this cluster won't be created."
   type        = bool
   default     = true
 }
 
-variable "service_account_email" {
-  description = "Optional GCE service account for node VMs (prefer a least-priv SA)"
+# Optional machine type override
+variable "node_machine_type" {
+  description = "Machine type for the GKE node pool."
   type        = string
-  default     = null
+  default     = "e2-standard-2"
 }
 
-variable "workload_identity_pool" {
-  description = "Optional workload identity pool in the form PROJECT.svc.id.goog (e.g. my-project.svc.id.goog)"
-  type        = string
-  default     = null
-}
-
-variable "enable_autoprovisioning" {
-  description = "Enable Node Autoprovisioning for cluster"
-  type        = bool
-  default     = false
-}
-
-variable "disk_size_gb" {
-  type    = number
-  default = 50
+# Optional node count override
+variable "node_count" {
+  description = "Number of nodes in the default node pool."
+  type        = number
+  default     = 2
 }
