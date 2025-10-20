be1c9dd81f5795bfbdfc5cc530bfe2d983ab8ce2
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:25:34 +0530

    Update outputs.tf

--- modules/project_services/outputs.tf
+++ modules/project_services/outputs.tf
@@ -1,9 +1,3 @@
-output "artifact_registry_repo" {
-  value = google_artifact_registry_repository.docker_repo.id
-}
-
-
-
-output "gke_service_account_email" {
-  value = google_service_account.gke_nodes.email
-}
+output "enabled" { 
+  value = true 
+  }
