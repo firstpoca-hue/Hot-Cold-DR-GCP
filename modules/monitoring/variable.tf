9f34ef29c4c643fe1c7140d42c5084476c6fcfd6
firstpoca-hue <firstpoca@gmail.com>
2025-10-20 11:22:57 +0530

    Update variable.tf

--- modules/monitoring/variable.tf
+++ modules/monitoring/variable.tf
@@ -20,3 +20,18 @@ variable "metric_threshold" {
   description = "CPU utilization threshold for alerting"
   default     = 80
 }
+
+
+variable "primary_lb_host"     { 
+type = string 
+} # hostname to probe
+variable "alert_channel_email" {
+type = string 
+}
+variable "run_job_name"        {
+ type = string 
+ }
+variable "run_job_region"      {
+type = string 
+}
+
