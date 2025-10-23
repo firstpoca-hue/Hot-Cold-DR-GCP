0f70e1836f2d24d5ffa0b9cb762bc0f92a84f4b6
firstpoca-hue <firstpoca@gmail.com>
2025-10-23 10:15:41 +0530

    Update outputs.tf

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -1,13 +1,23 @@
+##############################################
+# GKE Cluster Outputs (Safe for Conditional Count)
+##############################################
+
 output "cluster_name" {
-  value = google_container_cluster.gke_cluster.name
-  description = "GKE cluster name"
+  description = "Name of the GKE cluster"
+  value       = var.enable ? google_container_cluster.gke_cluster[0].name : null
 }
 
-output "endpoint" {
-  value = google_container_cluster.gke_cluster.endpoint
-  description = "GKE endpoint (private/public depending on cluster config)"
+output "cluster_location" {
+  description = "Region or zone where the GKE cluster is deployed"
+  value       = var.enable ? google_container_cluster.gke_cluster[0].location : null
 }
 
-output "location" {
-  value = google_container_cluster.gke_cluster.location
+output "cluster_endpoint" {
+  description = "GKE API endpoint (private/public depending on cluster config)"
+  value       = var.enable ? google_container_cluster.gke_cluster[0].endpoint : null
+}
+
+output "cluster_self_link" {
+  description = "Self link of the GKE cluster"
+  value       = var.enable ? google_container_cluster.gke_cluster[0].self_link : null
 }
